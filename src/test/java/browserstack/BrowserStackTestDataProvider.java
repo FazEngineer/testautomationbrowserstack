2b9f3333e21ea67023852dc4b37ff3cf14be3cfa
Khanfkf <Faisel.Khan1@rbs.co.uk>
2019-12-04 17:58:48 +0000

    update code

--- src/test/java/browserstack/BrowserStackTestDataProvider.java
+++ src/test/java/browserstack/BrowserStackTestDataProvider.java
@@ -14,10 +14,45 @@ import java.util.concurrent.TimeUnit;
 
 public class BrowserStackTestDataProvider {
 
-public WebDriver driver;
+    public WebDriver driver;
 
     public static String USERNAME = "fazshah1";
     public static String ACCESSKEY = "EXwzGn8tzopbHLdueBxT";
     public static String URL = "https://" + USERNAME + ":" + ACCESSKEY + "@hub.browserstack.com/wd/hub";
 
+
+    @Test (dataProvider = "browserStackTestData")
+    public void openSTM(Platform platform, String browserName, String browserVersion) throws MalformedURLException {
+
+        DesiredCapabilities capability = new DesiredCapabilities();
+        capability.setPlatform(platform);
+        capability.setBrowserName(browserName);
+        capability.setVersion(browserVersion);
+        capability.setCapability("browserstack.debug", false);
+
+        URL browserStackURL = new URL(URL);
+        //System.setProperty("webdriver.chrome.driver", "../testautomationbrowserstack/chromedriver");
+        WebDriver driver = new RemoteWebDriver(browserStackURL, capability);
+
+        driver.get("http://www.softwaretestingmaterial.com");
+        driver.manage().timeouts().implicitlyWait(3000, TimeUnit.SECONDS);
+        String actualURL = driver.getCurrentUrl();
+        String expectedURL = "https://www.softwaretestingmaterial.com/";
+        Assert.assertEquals(actualURL, expectedURL, "Actual and Expected are not the same");
+        driver.quit();
+    }
+
+    @DataProvider (name = "browserStackTestData", parallel = true)
+    public java.lang.Object[][] getData(){
+        java.lang.Object[][] testData = new Object[][] {
+                {Platform.MAC, "chrome", "62.0"},
+                {Platform.WIN8, "chrome", "62.0"},
+                {Platform.WINDOWS, "firefox", "57"},
+        };
+
+        return testData;
+    }
 }
+
+
+
